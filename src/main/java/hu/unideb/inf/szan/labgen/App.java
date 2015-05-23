2c8dd6bf09407912719741d505dca496dd5e80e0
Annamária Szentpéteri <szaea1994@gmail.com>
2015-05-23 13:03:52 +0200

    Getting started
    pom.xml under construction
    main structure of project done

---
+++ src/main/java/hu/unideb/inf/szan/labgen/App.java
@@ -0,0 +1,13 @@
+package hu.unideb.inf.szan.labgen;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
